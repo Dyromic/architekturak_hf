341db76b3646ae5fb39afcddfb892a1bd97ab668
Dyromic <programozo007@gmail.com>
2020-11-27 16:16:03 +0100

    Auth component done.

---
+++ services/auth/Auth/DTO/PasswordHash.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Auth.DTO
+{
+    public class PasswordHash
+    {
+
+        public string Password { get; set; }
+
+        public string Salt { get; set; }
+
+    }
+}
--- services/auth/Auth/Services/AuthService.cs
+++ services/auth/Auth/Services/AuthService.cs
@@ -1,22 +1,31 @@
 ﻿using Auth.DTO;
 using Auth.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using MongoDB.Bson;
 using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Net.Mail;
+using System.Security.Claims;
 using System.Security.Cryptography;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Auth.Services
 {
     public class AuthService : IAuthService
     {
-        private readonly IMongoCollection<UserEntity> _users;
 
-        public AuthService(IDatabaseSettings settings)
+        private readonly IMongoCollection<UserEntity> _users;
+        private readonly IConfiguration configuration;
+
+        public AuthService(IDatabaseSettings settings, IConfiguration configuration)
         {
+            this.configuration = configuration;
             var client = new MongoClient(settings.ConnectionString);
             var database = client.GetDatabase(settings.DatabaseName);
             _users = database.GetCollection<UserEntity>(settings.UsersCollectionName);
@@ -40,17 +49,77 @@ namespace Auth.Services
 
         public async Task<string> Register(UserRegistration user)
         {
+            if (!VerifyEmail(user.Email) || !VerifyPassword(user.Password)) return null;
             var foundUser = await _users.Find(u => u.Email == user.Email).FirstAsync();
             if (foundUser != null) {
                 return null;
             }
-            
-            throw new NotImplementedException();
+
+            PasswordHash passwordHash = CreatePasswordHash(user.Password);
+
+            UserEntity newUser = new UserEntity
+            {
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email,
+                PasswordHash = passwordHash.Password,
+                PasswordSalt = passwordHash.Salt
+            };
+
+            await _users.InsertOneAsync(newUser);
+
+            return GenerateJWT(newUser);
+
         }
 
-        private String GenerateJWT(UserEntity user) 
+        public bool VerifyEmail(string emailaddress)
         {
-            return "";
+            try
+            {
+                MailAddress m = new MailAddress(emailaddress);
+
+                return true;
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+        }
+
+        private bool VerifyPassword(string password)
+        {
+            Regex regex = new Regex(@"^(?=.*[a - z])(?=.*[A - Z])(?=.*\d)(?=.*[^\da - zA - Z]).{ 8, 25 }$");
+            return regex.Match(password).Success;
+        }
+
+        private String GenerateJWT(UserEntity user)
+        {
+
+            var mySecret = configuration["SecretKey"];
+            var mySecurityKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(mySecret));
+
+            var myIssuer = "";
+            var myAudience = "";
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.NameIdentifier, user.ID.ToString()),
+                    new Claim(ClaimTypes.Email, user.Email),
+                    new Claim(ClaimTypes.Surname, user.FirstName),
+                    new Claim(ClaimTypes.GivenName, user.LastName)
+                }),
+                Expires = DateTime.UtcNow.AddHours(12),
+                Issuer = myIssuer,
+                Audience = myAudience,
+                SigningCredentials = new SigningCredentials(mySecurityKey, SecurityAlgorithms.HmacSha512Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        
         }
 
         private string CreatePasswordHash(string password, string salt)
@@ -59,6 +128,17 @@ namespace Auth.Services
             using var hmac = new HMACSHA512(Encoding.UTF8.GetBytes(salt));
             var passwordInBytes = Encoding.UTF8.GetBytes(password);
             return Encoding.UTF8.GetString(hmac.ComputeHash(passwordInBytes));
+
+        }
+        private PasswordHash CreatePasswordHash(string password)
+        {
+
+            using var hmac = new HMACSHA512();
+            var passwordInBytes = Encoding.UTF8.GetBytes(password);
+            return new PasswordHash {
+                Password = Encoding.UTF8.GetString(hmac.ComputeHash(passwordInBytes)),
+                Salt = Encoding.UTF8.GetString(hmac.Key)
+            };
 
         }
     }
