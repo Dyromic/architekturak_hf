bb45b346969f5282b10e4826da1ca559a9247860
zihbot <zihbot@gmail.com>
2020-11-28 06:15:39 +0100

    status komponens kész

---
+++ services/status/Status/Controllers/StatusController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Status.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Status.Controllers
+{
+    [ApiController]
+    public class StatusController : ControllerBase
+    {
+        private readonly ILogger<StatusController> _logger;
+        private readonly StatusService _statusService;
+
+        public StatusController(ILogger<StatusController> logger, StatusService statusService)
+        {
+            _logger = logger;
+            _statusService = statusService;
+        }
+
+        [HttpGet("/status/{id}")]
+        public async Task<ActionResult<string>> Get(string id)
+        {
+            return await _statusService.Get(id);
+        }
+
+        [HttpPost("/status/{id}")]
+        public async Task<IActionResult> Post(string id, [FromForm] string status)
+        {
+            await _statusService.Put(id, status);
+            return Ok();
+        }
+    }
+}
---
+++ services/status/Status/Startup.cs
@@ -0,0 +1,61 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Status.Models;
+using Status.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Status
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<DatabaseSettings>(
+                Configuration.GetSection(nameof(DatabaseSettings)));
+            services.AddSingleton<DatabaseSettings>(sp =>
+                sp.GetRequiredService<IOptions<DatabaseSettings>>().Value);
+
+            services.AddSingleton<StatusService>();
+
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
