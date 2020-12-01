6732517bd340a17254da9c4808defae105a194eb
Dyromic <programozo007@gmail.com>
2020-12-01 04:37:15 +0100

    Production config.

--- app/src/config/config.ts
+++ app/src/config/config.ts
@@ -1,1 +1,2 @@
-export const ContentProviderAPI = 'https://localhost:27000/api/routes';
+//export const ContentProviderAPI = 'https://localhost:27000/api/routes';
+export const ContentProviderAPI = '/api/routes';
